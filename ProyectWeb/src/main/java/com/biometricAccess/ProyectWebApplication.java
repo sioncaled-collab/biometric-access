955e567e2321861857f794d30a5c694e60b826e3
sioncaled-collab <sioncaled@gmail.com>
2026-03-17 22:27:50 -0500

    Update ProyectWebApplication.java

--- ProyectWeb/src/main/java/com/biometricAccess/ProyectWebApplication.java
+++ ProyectWeb/src/main/java/com/biometricAccess/ProyectWebApplication.java
@@ -2,13 +2,13 @@ package com.biometricAccess;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.biometricAccess.repository")
 public class ProyectWebApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProyectWebApplication.class, args);
     }
 }
-
-
